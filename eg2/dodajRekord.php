4eb927d030f992491d543ab20c55645baf8c449c
B1ondyn <161232683+B1ondyn@users.noreply.github.com>
2024-02-25 20:09:49 +0100

    Add files via upload

---
+++ eg2/dodajRekord.php
@@ -0,0 +1,13 @@
+<?php
+    $polaczenie = mysqli_connect('localhost', 'root', '', 'ogloszenia');
+
+    $kategoria = $_POST["kategoria"];
+    $podkategoria = $_POST["podkategoria"];
+    $tytul = $_POST["tytul"];
+    $tresc = $_POST["tresc"];
+
+    $zapytanie = "INSERT INTO ogloszenie(id, uzytkownik_id, kategoria, podkategoria, tytul, tresc) VALUES ('', 1, $kategoria, $podkategoria, $tytul, $tresc);";
+    mysqli_query($polaczenie, $zapytanie);
+
+    mysqli_close($polaczenie);
+?>
